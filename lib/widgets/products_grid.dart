fd76646f981787adbc083118978d289814c09884
JRamos29 <malebolge@gmail.com>
2021-12-01 21:34:15 -0300

    Fixed ternary statement.

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -12,7 +12,7 @@ class ProductsGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = showFavs ? productsData.items : productsData.favoriteItems;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
